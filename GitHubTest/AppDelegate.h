b48ca034de4f23649380234a2b12f6573f817671
Pavan <pavm035@gmail.com>
2014-08-15 00:32:39 +0530

    Initial commit from local git

---
+++ GitHubTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by Pavan on 8/15/14.
+//  Copyright (c) 2014 Pavan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitHubTest
+//
+//  Created by Pavan on 8/15/14.
+//  Copyright (c) 2014 Pavan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
